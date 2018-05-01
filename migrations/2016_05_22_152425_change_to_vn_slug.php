fa63ba298f4f1dca16b6c347bd24a9a0aa2e234c
ganuonglachanh <ganuonglachanh@outlook.com>
2018-05-01 17:33:00 +0700

    remove database update

--- migrations/2016_05_22_152425_change_to_vn_slug.php
+++ migrations/2016_05_22_152425_change_to_vn_slug.php
@@ -5,22 +5,22 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->getConnection()->table('discussions')->chunk(100, function ($discussions) use ($schema) {
-            foreach ($discussions as $discussion) {
-                $schema->getConnection()->table('discussions')->where('id', $discussion->id)->update([
-                    'slug' => \GaNuongLaChanh\FlarumVnSlug\Utils::genSlug($discussion->title)
-                ]);
-            }
-        });
+//        $schema->getConnection()->table('discussions')->chunk(100, function ($discussions) use ($schema) {
+//            foreach ($discussions as $discussion) {
+//                $schema->getConnection()->table('discussions')->where('id', $discussion->id)->update([
+//                    'slug' => \GaNuongLaChanh\FlarumVnSlug\Utils::genSlug($discussion->title)
+//                ]);
+//            }
+//        });
     },
 
     'down' => function (Builder $schema) {
-        $schema->getConnection()->table('discussions')->chunk(100, function ($discussions) use ($schema) {
-            foreach ($discussions as $discussion) {
-                $schema->getConnection()->table('discussions')->where('id', $discussion->id)->update([
-                    'slug' => \Flarum\Util\Str::slug($discussion->title)
-                ]);
-            }
-        });
+//        $schema->getConnection()->table('discussions')->chunk(100, function ($discussions) use ($schema) {
+//            foreach ($discussions as $discussion) {
+//                $schema->getConnection()->table('discussions')->where('id', $discussion->id)->update([
+//                    'slug' => \Flarum\Util\Str::slug($discussion->title)
+//                ]);
+//            }
+//        });
     }
 ];
