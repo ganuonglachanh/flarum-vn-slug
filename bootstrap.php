9b58c2218f843fcf1250723ffabf49ae1ea814d6
ganuonglachanh <ganuonglachanh@outlook.com>
2018-05-01 17:10:07 +0700

    remove unuse code

--- bootstrap.php
+++ bootstrap.php
@@ -1,22 +1,19 @@
 <?php
 
-use Flarum\Event\ConfigureClientView;
+use Flarum\Event\ConfigureWebApp;
 use Flarum\Event\DiscussionWillBeSaved;
-use Flarum\Event\ExtensionWasEnabled;
-use Flarum\Extension\Extension;
+//use Flarum\Event\ExtensionWasEnabled;
+//use Flarum\Extension\Extension;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function(Dispatcher $events) {
-    $events->listen(ConfigureClientView::class, function(ConfigureClientView $event){
-        $event->addAssets(__DIR__.'/js/lib/string.js');
+    $events->listen(ConfigureWebApp::class, function(ConfigureWebApp $event){
+        //$event->addAssets(__DIR__.'/js/lib/string.js');
     });
 
     $events->listen(DiscussionWillBeSaved::class, function(DiscussionWillBeSaved $event){
 
         $text = $event->discussion->title;
-
-//        $slugtify = new \Cocur\Slugify\Slugify();
-//        $text = $slugtify->slugify($text);
 
         $text = \GaNuongLaChanh\FlarumVnSlug\Utils::genSlug($text);
 
